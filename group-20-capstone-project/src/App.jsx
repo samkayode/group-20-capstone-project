96d60a6b163e0e216e0218c7d6a71c25bfc2501c
kayode samuel <150209729+samkayode@users.noreply.github.com>
2026-06-03 19:06:55 +0100

    Completed planets section and mobile responsiveness for planets and header section

--- group-20-capstone-project/src/App.jsx
+++ group-20-capstone-project/src/App.jsx
@@ -1,11 +1,13 @@
 import Form from "./Form/form.jsx";
 import Footer from "./Footer/footer.jsx";
 import Header from "./Header/Header.jsx";
+import Planets from "./Planets/Planets.jsx";
 
 function App() {
   return (
     <>
       <Header />
+      <Planets />
       <Form />
       <Footer />
     </>
