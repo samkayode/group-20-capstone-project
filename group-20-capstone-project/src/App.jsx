da7f995b4cf2abd57fcd39d97b0601ef6a7cdad3
Triumph_1 <ajetomobibethel2009@gmail.com>
2026-06-04 09:43:25 +0100

    fixed video issue

--- group-20-capstone-project/src/App.jsx
+++ group-20-capstone-project/src/App.jsx
@@ -1,20 +1,17 @@
 import Form from "./Form/form.jsx";
 import Footer from "./Footer/footer.jsx";
 import Header from "./Header/Header.jsx";
-<<<<<<< HEAD
 import VideoSection from "./VideoSection/video.jsx";
-=======
 import Planets from "./Planets/Planets.jsx";
->>>>>>> 23c883fbd2e3385ae4a0c4a3189fdcb5e6d432c9
 
 function App() {
   return (
     <>
       <Header />
+      <VideoSection />
       <Planets />
       <Form />
       <Footer />
-      <VideoSection />
     </>
   );
 }
